1846b7f05734b458574a555372096be660d6b91a
GautamGolthi <30389900+GautamGolthi@users.noreply.github.com>
2023-06-09 17:34:53 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -8,17 +8,17 @@ terraform {
 }
 
 provider "aws" {
-region = "us-west-1"
+region = "ap-south-1"
 }
 resource "aws_instance" "myawsserver" {
-  ami = "ami-0f8e81a3da6e2510a"
+  ami = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
-  key_name = "san"
+  key_name = "gautam"
 
   tags = {
     Name = "Nginx-server"
     env = "Production"
-    owner = "Sandeep"
+    owner = "Gautam"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /etc/ansible/env"
